5b5502b6499756ff005a33c39c40ca6e11843212
Facundo Garcia <facundoxgc@gmail.com>
2020-03-29 23:03:07 -0400

    feat(software): Added the Bubble Sort algorithm for any type
    Added a new c++ script that implements the bubble sort algorithm using templates. It uses a vector as an input and outputs a sorted vector.
    
    Feat #1

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -14,54 +14,38 @@ ostream& operator<<(ostream& os, const vector<T>& v)
     } 
     os << "]\n"; 
     return os; 
-} 
-
-template <typename T> 
-void smallest(vector<T> &A, int &i, int &n, int &pos){
-    // Element to be inserted
-    T min = A[i];
-    
-    // Reposition the selector
-    pos = i;
-
-    // Locate position of smallest element
-    for (int j = i + 1; j < n; ++j) {
-        if (min > A[j]) {
-            min = A[j];
-            pos = j;
-        }  
-    }
 }
 
-template <typename T> 
-void selectionSort(vector<T> &A){ 
+template <typename T>
+void bubbleSort(vector<T> &A){ 
     // Calculate the size of the vector
     int n = A.size();
 
     // Initialize the variables
-    int pos = 0;
     T temp = 0;
 
     // Sort the values of the vector
     for (int i = 0; i < n; ++i) {
-        smallest(A, i, n, pos);
-        
-        // Swap with smallest element
-        temp = A[i];
-        A[i] = A[pos];
-        A[pos] = temp;
+        for (int j = i + 1; j < n; ++j) {
+            if (A[j] < A[i]) {
+                // Swap elements
+                temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
     }
 }
 
 int main(int, char*[]){
     // Insert the vector for testing
-    // vector<int> vec{ 13, 17, 2, 4, 11, 8, 19, 3, 7 };
+    // vector<int> vec{ 7, 11, 9, 1, 3, 18, 5, 21 };
     vector<double> vec{ 11.3, 17.1, 2.5, 4.4, 13, 8.1, 19.6, 3, 7.03 }; 
     
     // Print the original vector 
     cout << "Original vector: " << vec; 
 
-    selectionSort(vec);
+    bubbleSort(vec);
 
     // Print the resulting vector 
     cout << "Sorted vector: " << vec; 
